f30ffb508b453d60883ae5ca93acbe61601382c6
Scarlet Heavens <87530494+ScarletHeavens@users.noreply.github.com>
2021-11-22 17:17:14 +0100

    Update calc.js

--- calc.js
+++ calc.js
@@ -2,6 +2,10 @@ let current, result;
 let arr = [];
 let display = "";
 
+/*A function to perform basic math calculations. 
+I mapped the methods in an object, in order to avoid huge floats, limited to one digit after the point.
+**/
+
 let calculate = function (arr) {
   let mathOp = {
     addButton: function (x, y) {
@@ -15,24 +19,23 @@ let calculate = function (arr) {
     },
     divideButton: function (x, y) {
       return (x / y).toFixed(1);
-    },
+    }
   };
 
   return mathOp[arr[1]](Number(arr[0]), Number(arr[2]));
 };
 
 $(".number").click(function () {
-  if (arr.length == 1 || arr.length == 3) {
-    arr = [];
+  if (arr.length == 1 || arr.length == 3) { //a new operation 
     display = "";
+    arr = [];
   }
-
   display += $(this).val();
-  current = display;
+  current = display;  //keep the inserted num in current
   $("#display").val(display);
 });
 
-$("#clearButton").click(function () {
+$("#clearButton").click(function () { //brings all to initial state
   display = "";
   arr = [];
   current = "";
@@ -40,26 +43,26 @@ $("#clearButton").click(function () {
 });
 
 $(".operation").click(function () {
-  if (!arr.length && current != undefined) {
+  if (!arr.length && current != undefined) { //if current has a value, push the num and operand to the arr.
     arr = [current, this.id];
     current = undefined;
     display = "";
   }
 
   if (arr.length == 2) {
-    if (current == undefined) {
+    if (current == undefined) { // if two operands inserted in a row, choose the last one
       arr[1] = this.id;
     } else {
-      arr.push(current);
+      arr.push(current); //otherwise, push the current num, calculate the result.
       result = calculate(arr);
+      arr = [result, this.id];
       current = undefined;
-      stack = [result, this.id];
       display = "";
       $("#display").val(result);
     }
   }
 
-  if (arr.length == 3 && current == undefined) {
+  if (arr.length == 3 && current == undefined) { //if an operand comes after the last digit, calculate the prev result and continue. 
     result = calculate(arr);
     arr = [];
     display = "";
@@ -67,13 +70,13 @@ $(".operation").click(function () {
   }
 });
 
-$("#equalsButton").click(function () {
+$("#equalsButton").click(function () { //shows the result if the arr is complete.
   if (arr.length == 3) {
     result = calculate(arr);
     display = "";
     $("#display").val(result);
   }
-  if (arr.length == 2) {
+  if (arr.length == 2) { //if incomplete, check if there is a num in memo. Push it and calculate.
     if (Number(current)) {
       arr.push(current);
       current = undefined;
